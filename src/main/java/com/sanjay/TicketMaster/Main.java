11575374943228bba332a2cbeec15f0d862817b8
sanjay <44315700+sanjumsanthosh@users.noreply.github.com>
2023-12-21 21:43:05 +0530

    initial setup of gradle and class declaration

---
+++ src/main/java/com/sanjay/TicketMaster/Main.java
@@ -0,0 +1,30 @@
+package com.sanjay.TicketMaster;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // ticket booking
+
+//        TicketCounter ticketCounter = new TicketCounter();
+//        ticketCounter.anyShowsAvailable();
+//
+//
+//        User sanjay = new User("Sanjay M Santhosh", "Adult");
+//        User athulya = new User("Athulya", "Adult");
+//        User aadthiya = new User("Aadhitya", "Adult");
+//        User sanjana = new User("Sanjana M Santhosh", "Adult");
+//
+//        ticketCounter.bookSeat(sanjay);
+//        ticketCounter.bookSeat(athulya);
+//        ticketCounter.bookSeat(aadthiya);
+//        ticketCounter.bookSeat(sanjana);
+//
+//
+//        ticketCounter.whatAreAvailableFoods();
+//        ticketCounter.bookFood("Pizza", sanjay);
+//        ticketCounter.bookFood("Pizza", sanjana);
+//        ticketCounter.bookFood("Pop-corn", athulya);
+//        ticketCounter.bookFood("Pop-corn", athulya);
+    }
+
+}
