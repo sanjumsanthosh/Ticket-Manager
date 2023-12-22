abcb7634a8e32e4ad3e92342431f39f5c010861b
sanjay <44315700+sanjumsanthosh@users.noreply.github.com>
2023-12-22 22:20:17 +0530

    adding registering methods and whatShowRunning method

--- src/main/java/com/sanjay/TicketMaster/Screen.java
+++ src/main/java/com/sanjay/TicketMaster/Screen.java
@@ -1,11 +1,44 @@
 package com.sanjay.TicketMaster;
 
+import java.util.ArrayList;
+
 public class Screen implements IScreen {
 
+    // show and allSeat init
+    Show show;
+
+    Seat[] allSeats;
+
+
+    Screen(int seatTotalCount){
+        allSeats = new Seat[seatTotalCount];
+    }
+
+
+
+    // registerShow(show) -> this.show = show
+    // registerASeat(seat) -> allSeats appends seat
+
+    public void registerShow(Show show){
+        this.show = show;
+    }
+
+    public void registerASeat(Seat seat) {
+        allSeats[seat.seatNo-1] = seat;
+    }
 
     @Override
     public void whatShowRunning() {
-        System.out.println("JW4 is running");
+
+        // show details - JW4 running
+        show.printShowName();
+
+
+        // get me each seat "eachSeat" from "allSeats"
+        for (Seat eachSeat : allSeats) { //
+            eachSeat.printDetails();
+        }
+
     }
 
 }
