1e4813eced9fdbd64e03401491c64c2faebafb9c
sanjay <44315700+sanjumsanthosh@users.noreply.github.com>
2023-12-22 22:19:52 +0530

    registering seat and show

--- src/main/java/com/sanjay/TicketMaster/Main.java
+++ src/main/java/com/sanjay/TicketMaster/Main.java
@@ -3,8 +3,42 @@ package com.sanjay.TicketMaster;
 public class Main {
 
     public static void main(String[] args) {
+
         // ticket booking
-        Screen audi1 = new Screen();
+        Screen audi1 = new Screen(10);
+
+        // Show
+        Show johnwick4 = new Show("John Wick 4");
+
+
+        audi1.registerShow(johnwick4);
+
+        Seat seat1 = new Seat(1, "Balcony");
+        Seat seat2 = new Seat(2, "VIP");
+        Seat seat3 = new Seat(3, "Balcony");
+        Seat seat4 = new Seat(4, "Balcony");
+        Seat seat5 = new Seat(5, "VIP");
+        Seat seat6 = new Seat(6, "Balcony");
+        Seat seat7 = new Seat(7, "Balcony");
+        Seat seat8 = new Seat(8, "VIP");
+        Seat seat9 = new Seat(9, "Balcony");
+        Seat seat10 = new Seat(10, "Balcony");
+
+        audi1.registerASeat(seat1);
+        audi1.registerASeat(seat2);
+        audi1.registerASeat(seat3);
+        audi1.registerASeat(seat4);
+        audi1.registerASeat(seat5);
+        audi1.registerASeat(seat6);
+        audi1.registerASeat(seat7);
+        audi1.registerASeat(seat8);
+        audi1.registerASeat(seat9);
+        audi1.registerASeat(seat10);
+
+
+
+        //////---------------------------------------------------------------
+        ////// CUSTOMER FLOW -----------------------------
 
         TicketCounter ticketCounter = new TicketCounter(audi1);
         ticketCounter.anyShowsAvailable();
